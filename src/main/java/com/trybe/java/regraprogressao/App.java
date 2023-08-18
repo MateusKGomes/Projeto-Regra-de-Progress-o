8d5867d8af6a2921b376cf7db6b812db93a558a5
MateusKGomes <mateuskulik30@gmail.com>
2023-08-18 14:32:36 -0300

    Finaliza projeto

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,6 +1,7 @@
 package com.trybe.java.regraprogressao;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -27,30 +28,40 @@ public class App {
       System.out.println("Digite a nota obtida para " + atividades[i - 1] + ":");
       notas[i - 1] = Integer.parseInt(scanner.nextLine());
     }
+    float notaAluno = calculaNota(notas, pesos);
+    String resultadoAluno = resultado(notaAluno);
+    System.out.println(resultadoAluno);
     scanner.close();
   }
 
-  public static int calculaNota(int[] notas, int[] pesos) {
-    int notaFinal;
+  /**
+   * método calculaNota.
+   */
+  public static float calculaNota(int[] notas, int[] pesos) {
+    float notaFinal;
     int nota = 0;
     int peso = 0;
     for (int i = 0; i < notas.length; i++) {
       nota += notas[i] * pesos[i];
       peso += pesos[i];
     }
-    notaFinal = nota / peso;
+
+    notaFinal = (float) nota / peso;
     return notaFinal;
   }
 
-  public static String resutlado(int notaFinal) {
+  /**
+   * método resultado.
+   */
+  public static String resultado(float notaFinal) {
+    Locale.setDefault(Locale.US);
     if (notaFinal < 85) {
       return " Lamentamos informar que, com base na sua pontuação alcançada"
           + "neste período, " + notaFinal + "%, você não atingiu a"
           + "pontuação mínima necessária para sua aprovação.";
     } else {
-      return "Parabéns! Você alcançou " + notaFinal +
-          "%! E temos o prazer de informar que você obteve aprovação! ";
+      return "Parabéns! Você alcançou " + notaFinal
+          + "%! E temos o prazer de informar que você obteve aprovação! ";
     }
   }
-
 }
